f4e62b5667d3b665ab47323cd012eea203ac689c
Nasser <naser.abbasi447@gmail.com>
2017-06-27 01:28:20 +0200

    docs folder

--- docs/partials/slider.php
+++
@@ -1,9 +0,0 @@
-<div class="content" data-section="home">
-	<div id="large-header" class="large-header" style="background:#352f44;">
-		<canvas id="canvas"></canvas>
-		<div class="poly-text">
-		<h1 class="poly-title">For all THOSE transmission through <span class="thin">Clouds</span></h1>
-		<p>We are here to serve YOU	</p>
-		</div>
-	</div>
-</div>
