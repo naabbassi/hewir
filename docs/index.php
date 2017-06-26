0c33ef37f99a38bd26db2f46a26137b21b07104c
Nasser <naser.abbasi447@gmail.com>
2017-06-27 01:26:16 +0200

    docs folder

---
+++ docs/index.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+	<html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Hewir</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.C" />
+	<meta name="keywords" content="" />
+
+
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Canvas Style -->
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<?php 
+		include 'partials/header.php';
+		include 'partials/slider.php';
+		include 'partials/about.php';
+		include 'partials/services.php';
+		include 'partials/features.php';
+		include 'partials/testimonials.php';
+		include 'partials/contact.php';
+		include 'partials/footer.php';
+	
+	 ?>;
+	
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Polygone Background	 -->
+	<script src="js/TweenLite.min.js"></script>
+	<script src="js/EasePack.min.js"></script>
+	<script src="js/canvas.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ docs/partials/about.php
@@ -0,0 +1,26 @@
+<!--About us-->
+	<div id="fh5co-about-us" data-section="about">
+		<div class="container">
+			<div class="row row-bottom-padded-lg" id="about-us">
+				<div class="col-md-12 section-heading text-center">
+					<h2 class="to-animate">About Us</h2>
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 to-animate">
+							<h3>We connects you with an acclaimed freelance designer, freelance developer or small studio and a smart project management system created specifically for design and development projects. 99% of our projects have been completed on time, on budget and issue-free.</h3></div>
+					</div>
+				</div>
+				<div class="col-md-4 to-animate">
+					<h2>Design & Usability</h2>
+					<p>Great design is baked into everything we do. Great design is not only skin deep, but speaks into how something works. We believe in an iterative process for design and strive for feedback and validations every step of the way. Our passion for design excellence drives everything we produce. </p>
+				</div>
+				<div class="col-md-4 to-animate">
+					<h2>Technical Development</h2>
+					<p>Modern software development requires knowledge of many platforms. Our team of expert engineers is focused on the latest trends and technologies across the major web and mobile platforms. We’re no strangers to large architectures, third-party integrations and working side-by-side with our clients’ internal technology teams.</p>
+				</div>
+				<div class="col-md-4 to-animate">
+					<h2>Security & Encryption</h2>
+					<p>Protect and maintain your products in any scale of attacks is our skill , encrypting you data with trusted alghoritms and methods.</p>
+				</div>
+			</div>
+		</div>
+	</div>
