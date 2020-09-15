066b7ce4da7a20405659caca3307cf45c7d4b582
Nasser Abbassi <abbasiamanj@gmail.com>
2020-09-15 13:40:23 +0000

    rfactoring

--- partials/poly.php
+++
@@ -1,9 +0,0 @@
-<div class="content" data-section="poly">
-	<div id="large-header" class="large-header" style="background:#352f44;">
-		<canvas id="canvas"></canvas>
-		<div class="poly-text">
-		<h1 class="poly-title"><span class="thin">Unlock your digital business potential with</span> Hewir</h1>
-		<p>Yes, we can build that</p>
-		</div>
-	</div>
-</div>
